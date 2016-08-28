27a3c5db48dd56539f2d2397068542105756b1d1
Meruva Pradeep <meruva.pradeep@gmail.com>
2016-08-27 21:14:31 -0500

    static controller

---
+++ src/main/java/com/ryde/fm/controller/StaticController.java
@@ -0,0 +1,14 @@
+package com.ryde.fm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StaticController {
+
+	@RequestMapping({"/","/home"})
+public String getIndexPage(){
+	
+	return "static/index.html";
+}
+}

6abea93fd13c52b704edf839cc63189f72e34c24
Meruva Pradeep <meruva.pradeep@gmail.com>
2016-08-29 13:38:44 -0500

    Static data service added

--- src/main/java/com/ryde/fm/controller/FleetController.java
+++ src/main/java/com/ryde/fm/controller/FleetController.java
@@ -11,81 +11,105 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ryde.fm.domain.InsuranceMode;
+import com.ryde.fm.domain.Title;
+import com.ryde.fm.domain.Type;
 import com.ryde.fm.domain.Vehicle;
+import com.ryde.fm.domain.VehicleStatus;
 import com.ryde.fm.service.FleetService;
-
-
 
 @RestController
 public class FleetController {
 
-    @Autowired
-    FleetService fleetService;
-    
-    @RequestMapping(value = "/vehicles", method = RequestMethod.GET, produces = "application/json")
-    public List<Vehicle> getVehicles() {
-	
-	System.out.println("FleetController.getVehicles()");
+	@Autowired
+	FleetService fleetService;
 
-	return fleetService.getVehicles();
+	@RequestMapping(value = "/vehicles", method = RequestMethod.GET, produces = "application/json")
+	public List<Vehicle> getVehicles() {
 
-    }
-    
-    @RequestMapping(value = "/vehiclesByStatus/{status}", method = RequestMethod.GET, produces = "application/json")
-    public List<Vehicle> getVehiclesByStatus(@PathVariable("status") String status) {
-	
-	System.out.println("FleetController.getVehiclesByStatus()");
+		System.out.println("FleetController.getVehicles()");
 
-	return fleetService.fetchByStatus(status);
+		return fleetService.getVehicles();
 
-    }
-    
-    @RequestMapping(value = "/vehiclesByType/{type}", method = RequestMethod.GET, produces = "application/json")
-    public List<Vehicle> getVehiclesByType(@PathVariable("type") String type) {
-	
-	System.out.println("FleetController.getVehiclesByStatus()");
+	}
 
-	return fleetService.fetchByType(type);
+	@RequestMapping(value = "/vehiclesByStatus/{status}", method = RequestMethod.GET, produces = "application/json")
+	public List<Vehicle> getVehiclesByStatus(@PathVariable("status") String status) {
 
-    }
+		System.out.println("FleetController.getVehiclesByStatus()");
 
-    @RequestMapping(value = "/vehicles", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
-	
-	Vehicle createdVehicle =  fleetService.createVehicle(vehicle);
+		return fleetService.fetchByStatus(status);
 
-	return new ResponseEntity<Vehicle>(createdVehicle, HttpStatus.CREATED);
+	}
 
-    }
-    
-    @RequestMapping(value = "/vehicles/{id}", method = RequestMethod.PUT, produces = "application/json")
-    public ResponseEntity<Vehicle> createVehicle(@PathVariable("id") long id,@RequestBody Vehicle vehicle) throws Exception {
-	
-	Vehicle updatedVehicle =  fleetService.updateVehicle(id,vehicle);
-	
+	@RequestMapping(value = "/vehiclesByType/{type}", method = RequestMethod.GET, produces = "application/json")
+	public List<Vehicle> getVehiclesByType(@PathVariable("type") String type) {
 
-	return new ResponseEntity<Vehicle>(updatedVehicle, HttpStatus.ACCEPTED);
+		System.out.println("FleetController.getVehiclesByStatus()");
 
-    }
-    
-    @RequestMapping(value = "/vehicles/{id}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<Vehicle> getVehicle(@PathVariable("id") long id) throws Exception {
-	
-	Vehicle vehicle =  fleetService.getVehicleById(id);
-	
+		return fleetService.fetchByType(type);
 
-	return new ResponseEntity<Vehicle>(vehicle, HttpStatus.OK);
+	}
 
-    }
-    
-    @RequestMapping(value = "/vehicles/{id}", method = RequestMethod.DELETE, produces = "application/json")
-    public ResponseEntity deleteVehicle(@PathVariable("id") long id) throws Exception {
-	
-	 fleetService.deleteVehicle(id);
-	
+	@RequestMapping(value = "/vehicles", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
 
-	return new ResponseEntity(HttpStatus.ACCEPTED);
+		Vehicle createdVehicle = fleetService.createVehicle(vehicle);
 
-    }
-    
+		return new ResponseEntity<Vehicle>(createdVehicle, HttpStatus.CREATED);
+
+	}
+
+	@RequestMapping(value = "/vehicles/{id}", method = RequestMethod.PUT, produces = "application/json")
+	public ResponseEntity<Vehicle> createVehicle(@PathVariable("id") long id, @RequestBody Vehicle vehicle)
+			throws Exception {
+
+		Vehicle updatedVehicle = fleetService.updateVehicle(id, vehicle);
+
+		return new ResponseEntity<Vehicle>(updatedVehicle, HttpStatus.ACCEPTED);
+
+	}
+
+	@RequestMapping(value = "/vehicles/{id}", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Vehicle> getVehicle(@PathVariable("id") long id) throws Exception {
+
+		Vehicle vehicle = fleetService.getVehicleById(id);
+
+		return new ResponseEntity<Vehicle>(vehicle, HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "/vehicles/{id}", method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity deleteVehicle(@PathVariable("id") long id) throws Exception {
+
+		fleetService.deleteVehicle(id);
+
+		return new ResponseEntity(HttpStatus.ACCEPTED);
+
+	}
+
+	@RequestMapping(value = "/vehicleTypes", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Type[]> getVehicleTypes() {
+
+		return new ResponseEntity<Type[]>(Type.values(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/titleTypes", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Title[]> getTitleTypes() {
+
+		return new ResponseEntity<Title[]>(Title.values(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/vehicleStatusValues", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<VehicleStatus[]> getVehicleStatusValues() {
+
+		return new ResponseEntity<VehicleStatus[]>(VehicleStatus.values(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/insuranceModeValues", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<InsuranceMode[]> getInsuranceModeValues() {
+
+		return new ResponseEntity<InsuranceMode[]>(InsuranceMode.values(), HttpStatus.OK);
+	}
+
 }

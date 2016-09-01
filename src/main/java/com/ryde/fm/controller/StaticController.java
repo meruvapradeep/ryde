75a112cec0b4ff8cb69b21b55365e81529669662
Meruva Pradeep <meruva.pradeep@gmail.com>
2016-09-01 14:20:39 -0500

    formated

--- src/main/java/com/ryde/fm/controller/StaticController.java
+++ src/main/java/com/ryde/fm/controller/StaticController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class StaticController {
 
-	@RequestMapping({"/","/home"})
-public String getIndexPage(){
-	
-	return "index.html";
-}
+	@RequestMapping({ "/", "/home" })
+	public String getIndexPage() {
+
+		return "index.html";
+	}
 }
